1ba67d75551c1598f821c24d66e9412d214a07e4
ferza17 <feryreza85@gmail.com>
2021-01-11 13:29:29 +0700

    error handling

--- calculator/calculator_server/server.go
+++ calculator/calculator_server/server.go
@@ -6,6 +6,7 @@ import (
 	"github.com/ferza17/grpc-course/calculator/calculatorpb"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/reflection"
 	"google.golang.org/grpc/status"
 	"io"
 	"log"
@@ -127,6 +128,7 @@ func main() {
 	}
 	s := grpc.NewServer()
 	calculatorpb.RegisterSumServiceServer(s, &server{})
+	reflection.Register(s)
 
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("Failed to server: %v", err)
